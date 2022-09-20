761c01a36a70aa373491ed29ee8566dfd6300498
Thais Almeida <90707459+thais36@users.noreply.github.com>
2022-09-20 18:21:42 -0300

    Add files via upload

---
+++ Estrutura-de-Dados-2-semestre-main/EstruturaEstatica/src/main/java/com/mycompany/estruturaestatica/Teste.java
@@ -0,0 +1,11 @@
+package com.mycompany.estruturaestatica;
+
+public class Teste {
+   //testar o projeto 
+    public static void main(String[] args){
+        Pilha<Integer> pilha = new Pilha<Integer>();
+        
+        System.out.println(pilha);
+        System.out.println(pilha.tamanho());
+    }    
+}
